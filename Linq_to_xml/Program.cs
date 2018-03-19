7ff299cfd591b89319d1a7fd5e5cbed39520a257
Maciej Wicher <wicher.maciej@gmail.com>
2018-03-19 17:13:29 +0100

    Add project files.

---
+++ Linq_to_xml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_to_xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
